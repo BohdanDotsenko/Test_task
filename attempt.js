64a3664d94f6a3660cb2e97cef8768977a84f6b1
BohdanDotsenko <d.bogdan09@gmail.com>
2020-05-05 14:42:17 +0300

    'final'

--- attempt.js
+++ attempt.js
@@ -5,7 +5,8 @@ function attempt(available, allowed, preferred) {
         return [];
       }
     if (!allowed.includes('any')){    
-        for (let i = 0; i < available.length; i++) {    //fill the array valid(allowed and available numbers)
+        for (let i = 0; i < available.length; i++) {    
+            //fill the array valid(allowed and available numbers)
             if (allowed.includes(available[i])){
                 valid.push(available[i]);
             }
@@ -13,11 +14,13 @@ function attempt(available, allowed, preferred) {
     }
     else                            // if 'any' element is found in allowed than available == valid
         valid = available;          
-    if (preferred.includes('any')){ // if 'any' element is found in preferred than all valid numbers are preferred
+    if (preferred.includes('any')){ 
+        // if 'any' element is found in preferred than all valid numbers are preferred
         return valid;
     }
     else {
-        let notPreferrad = []    // all not preferred but valid numners
+        let notPreferrad = []    
+        // all not preferred but valid numners
         for( let i = 0; i < preferred.length; i++) { // push all preferred and valid numbers
             if(valid.includes(preferred[i]))
                 result.push(preferred[i]);
@@ -25,7 +28,8 @@ function attempt(available, allowed, preferred) {
                 notPreferrad.push(preferred[i])
         }
             let buffer = Infinity;
-            if(notPreferrad.length == 1) {     // if we have one not preferred number find close in value or min valid number
+            if(notPreferrad.length == 1) {     
+                // if we have one not preferred number find close in value or min valid number
                     for(let i = 0; i < valid.length; i++) {  // 
                         if(!preferred.includes(valid[i])) 
                             buffer = valid[i]
@@ -41,7 +45,8 @@ function attempt(available, allowed, preferred) {
                     if (buffer != Infinity)
                         result.push(buffer);
             }
-            else if(notPreferrad.length > 1) { // if we have more than one not preferred numbers we will find max not preferred valid number
+            else if(notPreferrad.length > 1) { 
+                // if we have more than one not preferred numbers we will find max not preferred valid number
                 for(let i = 0; i < valid.length; i++) {
                     if (!preferred.includes(valid[i]))
                         buffer = valid[i];
